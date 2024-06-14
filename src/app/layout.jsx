cd0edd9cdb6691cba43b2effdeb838985a96ea01
fuadinadhif <nadhiffuadi@gmail.com>
2024-06-14 20:05:26 +0700

    tidy up the layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 
-import Logo from "@/components/Logo";
-import Menu from "@/components/Menu";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,42 +20,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} dark:bg-neutral-950 dark:text-white`}
       >
-        <header className="mx-auto max-w-screen-xl p-6">
-          <div className="hidden xl:block">
-            <div>
-              <Logo className={"text-center text-7xl"} />
-              <h2 className="mt-2 text-center text-2xl uppercase leading-none">
-                Fullfill
-                <br />
-                <span className="font-extralight">your</span> belly
-              </h2>
-            </div>
-            <Menu className="mt-8 flex items-center justify-center gap-4" />
-          </div>
-          <div className="hidden items-center justify-between md:flex xl:hidden">
-            <Logo className="text-3xl" />
-            <Menu className="flex items-center justify-center gap-4" />
-          </div>
-          <div className="flex justify-between md:hidden">
-            <Logo className="text-3xl" />
-            <button class="flex items-center rounded border-2 border-black px-3 py-2">
-              <svg
-                class="h-3 w-3 fill-current"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-        </header>
+        <Header className="mx-auto max-w-screen-xl p-6" />
         <main className="mx-auto max-w-screen-xl p-6">{children}</main>
-        <footer className="mt-24 text-center">
-          <p className="bg-neutral-950 py-6 text-white">
-            © 2024 FullBelly ❤️{" "}
-          </p>
-        </footer>
+        <Footer className="mt-24 text-center" />
       </body>
     </html>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer({ className }) {
+  return (
+    <footer className={className}>
+      <p className="bg-neutral-950 py-6 text-white">© 2024 FullBelly ❤️ </p>
+    </footer>
+  );
+}
